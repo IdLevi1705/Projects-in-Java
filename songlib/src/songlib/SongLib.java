fa60bf9ccf8be67e17fdf04badd2b10eb1e854e4
idanlevi <idanlevi@nbp-243-244.nbp.ruw.rutgers.edu>
2020-02-06 14:25:26 -0500

    first setup

---
+++ songlib/src/songlib/SongLib.java
@@ -0,0 +1,23 @@
+package songlib;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class SongLib extends Application{
+	
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        primaryStage.setTitle("Hello World");
+        primaryStage.show();
+    }
+	
+	public static void main (String[] args) {
+		launch(args);
+	}
+
+	
+}
